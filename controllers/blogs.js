4dedb4b68c3fd955f56299e9977a133bffbbb25c
MacielDouglas <douglasmaciel.dm@gmail.com>
2023-05-13 08:14:40 -0300

    Retornado tres commits e adicionando o teardow.js para erros de versão do mongoose

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -7,29 +7,16 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs);
 });
 
-blogsRouter.get('/:id', async (request, response, next) => {
-  try {
-    const blog = await Blog.findById(request.params.id);
-    if (blog) {
-      response.json(blog);
-    } else {
-      response.status(404).end();
-    }
-  } catch (exception) {
-    next(exception);
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.status(404).end();
   }
-  // Blog.findById(request.params.id)
-  //   .then((blog) => {
-  //     if (blog) {
-  //       response.json(blog);
-  //     } else {
-  //       response.status(404).end();
-  //     }
-  //   })
-  //   .catch((error) => next(error));
 });
 
-blogsRouter.post('/', async (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body;
 
   const blog = new Blog({
@@ -38,20 +25,14 @@ blogsRouter.post('/', async (request, response, next) => {
     url: body.url,
     likes: body.likes,
   });
-  try {
-    const savedBlog = await blog.save();
-    response.status(201).json(savedBlog);
-  } catch (exception) {
-    next(exception);
-  }
+
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
 });
 
-blogsRouter.delete('/:id', (request, response, next) => {
-  Blog.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
 });
 
 blogsRouter.put('/:id', (request, response, next) => {
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -7,15 +7,15 @@ const app = require('../app');
 const api = supertest(app);
 
 const Blog = require('../models/blog');
+const blog = require('../models/blog');
 
 beforeEach(async () => {
   await Blog.deleteMany({});
 
-  let blogObject = new Blog(helper.initialBlogs[0]);
-  await blogObject.save();
-
-  blogObject = new Blog(helper.initialBlogs[1]);
-  await blogObject.save();
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog);
+    await blogObject.save();
+  }
 });
 
 //teste a deletar depois, apenas seguindo os passos vvvvv
@@ -67,31 +67,19 @@ test('blog without title is not add', async () => {
 });
 
 // Teste que busca e apaga um blog individual
-test('a specific note can be viewed', async () => {
+test('a specific blog can be viewed', async () => {
   const blogsAtStart = await helper.blogsInDb();
 
   const blogToView = blogsAtStart[0];
+  // console.log(blogToView._id);
 
   const resultBlog = await api
-    .get(`/api/notes/${blogToView.id}`)
+    .get(`/api/blogs/${blogToView.id}`)
     .expect(200)
     .expect('Content-Type', /application\/json/);
 
   expect(resultBlog.body).toEqual(blogToView);
 });
-
-// test('a specific blog can be viewed', async () => {
-//   const blogsAtStart = await helper.blogsInDb();
-
-//   const blogToView = blogsAtStart[0];
-
-//   const resultBlog = await api
-//     .get(`/api/blogs/${blogToView.id}`)
-//     .expect(200)
-//     .expect('Content-Type', /application\/json/);
-
-//   expect(resultBlog.body).toEqual(blogToView);
-// });
 
 test('a blog can be deleted', async () => {
   const blogsAtStart = await helper.blogsInDb();
