d1f3e9eaca2f1b1cc55cb85465ca9fbf45594c2d
MacielDouglas <douglasmaciel.dm@gmail.com>
2023-05-20 15:24:09 -0300

    Adicionado login de usuario e Post via token. Token tem duração de 60min

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
 const userRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 mongoose.set('strictQuery', false);
 
@@ -29,6 +30,7 @@ app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', userRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
